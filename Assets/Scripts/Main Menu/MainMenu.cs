df12600e215fe039c882266f9c41dc27311ac6bb
Rayyan Aziz <itsRayan06@gmail.com>
2022-08-24 19:17:01 +0500

    UI done duz duz

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,4 +1,6 @@
 using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,22 +12,33 @@ namespace XGStudios.MainMenu
     {
         [Header("Strings")]
         public string gameScene;
+
+        [Header("Texts")]
+        public TextMeshProUGUI leaderBoardTxt;
         
         [Header("Menu Buttons")]
         public Button playButton;
         public Button settingsButton;
         public Button creditsButton;
         public Button aboutUsButton;
+        public Button leaderBoardBtn;
         public Button quitButton;
         
         [Space(5f)]
         [Header("UI Panels")]
-        public GameObject settingsPanel;
-        public GameObject creditsPanel;
-        public GameObject aboutUsPanel;
+        public CanvasGroup settingsCanvas;
+        public RectTransform settingsRect;
+        public CanvasGroup creditsCanvas;
+        public RectTransform creditsRect;
+        public CanvasGroup aboutUsCanvas;
+        public RectTransform aboutUsRect;
+        public CanvasGroup leaderBoardCanvas;
+        public RectTransform leaderBoardRect;
 
         void Start()
         {
+            leaderBoardTxt.SetText("LEADERBOARD");
+            
             if (playButton != null)
             {
                 playButton.onClick.AddListener(() => {
@@ -36,21 +49,49 @@ namespace XGStudios.MainMenu
             if (settingsButton != null)
             {
                 settingsButton.onClick.AddListener(() => {
-                    settingsPanel.SetActive(true);
+                    settingsCanvas.alpha = 0;
+                    settingsCanvas.interactable = true;
+                    settingsCanvas.blocksRaycasts = true;
+                    settingsRect.transform.localPosition = new Vector3(0, -1000f, 0);
+                    settingsRect.DOAnchorPos(new Vector2(0,0), 0.2f, false).SetEase(Ease.OutExpo);
+                    settingsCanvas.DOFade(1, 0.2f);
                 });
             }
             
             if (creditsButton != null)
             {
                 creditsButton.onClick.AddListener(() => {
-                    creditsPanel.SetActive(true);
+                    creditsCanvas.alpha = 0;
+                    creditsCanvas.interactable = true;
+                    creditsCanvas.blocksRaycasts = true;
+                    creditsRect.transform.localPosition = new Vector3(0, -1000f, 0);
+                    creditsRect.DOAnchorPos(new Vector2(0,0), 0.2f, false).SetEase(Ease.OutExpo);
+                    creditsCanvas.DOFade(1, 0.2f);
                 });
             }
             
             if (aboutUsButton != null)
             {
                 aboutUsButton.onClick.AddListener(() => {
-                    aboutUsPanel.SetActive(true);
+                    aboutUsCanvas.alpha = 0;
+                    aboutUsCanvas.interactable = true;
+                    aboutUsCanvas.blocksRaycasts = true;
+                    aboutUsRect.transform.localPosition = new Vector3(0, -1000f, 0);
+                    aboutUsRect.DOAnchorPos(new Vector2(0,0), 0.2f, false).SetEase(Ease.OutExpo);
+                    aboutUsCanvas.DOFade(1, 0.2f);
+                });
+            }
+            
+            if (leaderBoardBtn != null)
+            {
+                leaderBoardBtn.onClick.AddListener(() => {
+                    leaderBoardTxt.SetText("REFRESH");
+                    leaderBoardCanvas.alpha = 0;
+                    leaderBoardCanvas.interactable = true;
+                    leaderBoardCanvas.blocksRaycasts = true;
+                    leaderBoardRect.transform.localPosition = new Vector3(600, -1000f, 0);
+                    leaderBoardRect.DOAnchorPos(new Vector2(0,0), 0.2f, false).SetEase(Ease.OutExpo);
+                    leaderBoardCanvas.DOFade(1, 0.2f);
                 });
             }
             
@@ -60,16 +101,35 @@ namespace XGStudios.MainMenu
 
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (!Input.GetKeyDown(KeyCode.Escape))return;
+            
+            if (settingsCanvas.alpha != 0)
             {
-                if (settingsPanel.activeSelf)
-                    settingsPanel.SetActive(false);
-                if (creditsPanel.activeSelf)
-                    creditsPanel.SetActive(false);
-                if (aboutUsPanel.activeSelf)
-                    aboutUsPanel.SetActive(false);
+                settingsCanvas.alpha = 1;
+                settingsCanvas.interactable = false;
+                settingsCanvas.blocksRaycasts = false;
+                settingsRect.transform.localPosition = new Vector3(0, 0, 0);
+                settingsRect.DOAnchorPos(new Vector2(0,-1000f), 0.2f, false).SetEase(Ease.OutExpo);
+                settingsCanvas.DOFade(0, 0.2f);
             }
-
+            if (creditsCanvas.alpha != 0)
+            {
+                creditsCanvas.alpha = 1;
+                creditsCanvas.interactable = false;
+                creditsCanvas.blocksRaycasts = false;
+                creditsRect.transform.localPosition = new Vector3(0, 0, 0);
+                creditsRect.DOAnchorPos(new Vector2(0,-1000f), 0.2f, false).SetEase(Ease.OutExpo);
+                creditsCanvas.DOFade(0, 0.2f);
+            }
+            if (aboutUsCanvas.alpha != 0)
+            {
+                aboutUsCanvas.alpha = 1;
+                aboutUsCanvas.interactable = false;
+                aboutUsCanvas.blocksRaycasts = false;
+                aboutUsRect.transform.localPosition = new Vector3(0, 0, 0);
+                aboutUsRect.DOAnchorPos(new Vector2(0,-1000f), 0.2f, false).SetEase(Ease.OutExpo);
+                aboutUsCanvas.DOFade(0, 0.2f);
+            }
         }
     }
 }
--- Assets/Scripts/Main Menu/SettingsMenu.cs
+++ Assets/Scripts/Main Menu/SettingsMenu.cs
@@ -1,11 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace XGStudios.MainMenu
 {
     public class SettingsMenu : MonoBehaviour
     {
+        public TMP_Dropdown resolutionDropdown;
+                public Toggle fullScreenToggle;
+                
+                Resolution[] _resolutions;
+                int _screenInt;
+                const String _resName = "resolutionOption";
         
+                void Awake()
+                {
+                    _screenInt = PlayerPrefs.GetInt("ToggleState");
+        
+                    if (_screenInt == 1)
+                    {
+                        fullScreenToggle.isOn = true;
+                    }
+                    else
+                        fullScreenToggle.isOn = false;
+                    
+                    resolutionDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+                    {
+                        PlayerPrefs.SetInt(_resName, resolutionDropdown.value);
+                        PlayerPrefs.Save();
+                    }));
+                }
+        
+                void Start()
+                {
+                    _resolutions = Screen.resolutions;
+                    resolutionDropdown.ClearOptions();
+                    
+                    List<String> options = new List<string>();
+        
+                    int currResIndex = 0;
+                    for (int i = 0; i < _resolutions.Length; i++)
+                    {
+                        string option = _resolutions[i].width + " x " + _resolutions[i].height + " - " + _resolutions[i].refreshRate + " hz";
+                        options.Add(option);
+        
+                        if (_resolutions[i].width == Screen.currentResolution.width &&
+                            _resolutions[i].height == Screen.currentResolution.height)
+                            currResIndex = i;
+                    }
+                    
+                    resolutionDropdown.AddOptions(options);
+                    resolutionDropdown.value = PlayerPrefs.GetInt(_resName, currResIndex);
+                    resolutionDropdown.RefreshShownValue();
+                }
+        
+                public void SetFullScreen(bool isFullScreen)
+                {
+                    Screen.fullScreen = isFullScreen;
+                    
+                    if (isFullScreen == false)
+                        PlayerPrefs.SetInt("ToggleState", 0);
+                    else
+                    {
+                        isFullScreen = true;
+                        PlayerPrefs.SetInt("ToggleState", 1);
+                    }
+                }
+                
+                public void SetResolution(int resolutionIndex)
+                {
+                    Resolution resolution = _resolutions[resolutionIndex];
+                    Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+                }
     }
 }
